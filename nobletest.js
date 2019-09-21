991abf0cd297e25c2698690900153490847870e8
don undeen <donundeen@gmail.com>
2019-09-21 18:03:57 -0400

    finisehd basic noble test

--- nobletest.js
+++ nobletest.js
@@ -1,8 +1,11 @@
 var noble = require("noble");
 
-const serviceUUIDs = ['aa40'];
+
+var theService = 'aa40';
+var theCharacteristic = 'aa41';
+const serviceUUIDs = [theService];
 //const characteristicUUIDs = ['aa41'];
-const characteristicUUIDs = [];
+const characteristicUUIDs = [theCharacteristic];
 
 noble.on("stateChange", function(state){
     console.log("noble state change");
@@ -47,7 +50,29 @@ function connectAndSetUp(peripheral){
 
 function onServicesAndCharacteristicsDiscovered(error, services, characteristics){
     console.log("discovered services and characteristics");
+    console.log("services are");
     console.log(services);
+    console.log("characteristics are");
     console.log(characteristics);
+    var readings = characteristics.filter(function(Charact){
+        if (Charact.uuid == theCharacteristic){
+            return true;
+        }
+        return false;
+    })[0];
+    console.log(readings);
+    readings.on("read", function(data, isNotification){
+        console.log("reading");
+        console.log(isNotification);
+        console.log(data);
+    });
+
+    function doReading(){
+        readings.read();
+        setTimeout(doReading, 150);    
+    }    
+    doReading();
 
 }
+
+
