1b0b31be22f0a3d4f458b2e8226fe01b60f105a9
don undeen <donundeen@gmail.com>
2019-11-02 13:16:03 -0400

    supporting any number of devices

--- vchorus.js
+++ vchorus.js
@@ -1,21 +1,31 @@
 var noble = require("noble");
 var OSC = require("osc-js");
+
+
 const osc = new OSC({ plugin: new OSC.DatagramPlugin() })
 //osc.open({ port: 9912 })
 var host = "localhost";
-// host = "192.168.1.107";
-var devices = {"939443f4bbaa4c60a9c4b98d7f406723" : {
-                    name: "granite",
-                    connected : false}, 
-               "e293363ad8414e9f8e1df47d4b9f0ecd" : {
-                    name: "oak",
-                    connected : false}
+var port = 9002
+
+var deviceNames = {
+                "939443f4bbaa4c60a9c4b98d7f406723" :  "granite",
+                "e293363ad8414e9f8e1df47d4b9f0ecd" :  "oak"
                 };
 
-var numDevices = Object.keys(devices).length;
+// host = "192.168.1.107";
+// list of devices with their known names.
+var devices = {};
+// 
+var deviceTemplate = {
+    name :false,
+    connected : false
+}
+
+var scanTimeMS = 5000;
+
 var numConnected = 0;
 
-osc.open({ host: host, port: 9002 })
+osc.open({ host: host, port: port })
 
 
 var theService = 'aa40';
@@ -30,7 +40,6 @@ noble.on("stateChange", function(state){
     console.log(state);
     console.log(serviceUUIDs);
 
-    
     if (state === 'poweredOn'){
 	   noble.startScanning(serviceUUIDs, true);
 //	noble.startScanning();
@@ -41,31 +50,48 @@ noble.on("stateChange", function(state){
 
 
 noble.on('discover', function(peripheral){
-    if(!devices[peripheral.id].connected){
-        devices[peripheral.id].connected = true;
-        devices[peripheral.id].peripheral = peripheral;
-        numConnected++;
-        console.log("peripheral discovered");
-        console.log(peripheral);
-    }else{
-        console.log(peripheral.id + " already connected");
-    }
-    console.log("compare " + numConnected + " to "+ numDevices);
-    if(numConnected == numDevices){
-        noble.stopScanning();
-        var ids = Object.keys(devices);
-        console.log(ids);
-        for(var i = 0; i < ids.length; i++){
-            connectAndSetUp(devices[ids[i]].peripheral);
+    (function(_peripheral){
+        if(!devices[_peripheral.id]){
+            devices[_peripheral.id] = JSON.parse(JSON.stringify(deviceTemplate));
         }
-    }
+                
+        if(!devices[_peripheral.id].connected){
+            console.log("peripheral discovered");
+            console.log(_peripheral.id);
+            devices[_peripheral.id].connected = true;
+            devices[_peripheral.id].peripheral = _peripheral;
+            if(deviceNames[_peripheral.id]){
+                devices[_peripheral.id].name = deviceNames[_peripheral.id];
+            }else{
+                devices[_peripheral.id].name = _peripheral.id;
+            }
+        }else{
+            console.log(_peripheral.id + " already connected");
+//            console.log(devices);
+        }
 
+    })(peripheral);
 });
 
 
+// scan for devices for 5  seconds, then set them all up.
+setTimeout(setupAllDevices, scanTimeMS);
+
+
+function setupAllDevices(){
+    console.log("connect and setup all devices");
+//    console.log(devices);
+    noble.stopScanning();
+    var ids = Object.keys(devices);
+    console.log(ids);
+    for(var i = 0; i < ids.length; i++){
+        connectAndSetUp(devices[ids[i]].peripheral);
+    }
+}
+
+
 function connectAndSetUp(peripheral){
-    console.log("connect and setup");
-    console.log(peripheral);
+//    console.log(peripheral);
     peripheral.connect(error => {
         if(peripheral.setup){
             return;
@@ -127,7 +153,7 @@ function sendData(data, device){
     console.log("B: " + valueInt2);
 
     var message = new OSC.Message('/perifit/1', valueInt1, valueInt2, device.name);
-    osc.send(message, {  host: host, port: 9002 });  
+    osc.send(message, {  host: host, port: port });  
 }
 
 
